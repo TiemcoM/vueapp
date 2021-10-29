8d16cb3b2816e7238cb8ee69089470f2dabd5b95
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-10-29 15:02:28 +0200

    Login works a lot better now. Api token is being stored

--- src/components/login.vue
+++ src/components/login.vue
@@ -23,81 +23,45 @@
     </v-card-text>
     <v-divider></v-divider>
     <v-card-actions>
-      <v-btn color="info" @click="handleLogin">Login</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn color="message" @click="clearLogin">clear</v-btn>
+      <v-btn color="info" @click="login">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-import axios from "axios";
-
 export default {
   name: "Inlog",
 
-  data() {
+  data () {
     return {
-      dialogInlog: false,
       showPassword: false,
-
-      email: "",
-      password: "",
-
-      info_login: null,
-    };
-  },
-
-  created: function () {
-    const name = "info_login_cookie";
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    let ret = null;
-    if (parts.length === 2) {
-      ret = parts.pop().split(";").shift();
-      ret = JSON.parse(ret);
-    }
-    this.$store.state.inlog_data = ret;
-    this.info_login = ret;
-  },
-  mounted() {
-    if (this.info_login == "leeg") {
-      console.log("cookie info_login from component login.vue is leeg");
-    } else {
-      this.$store.state.info_login = this.info_login;
+      email: '',
+      password: ''
     }
   },
   methods: {
-    handleLogin: function () {
-      console.log("this.email=", this.email);
-      console.log("this.password=", this.password);
-      // let self = this;
-      // let deState = this.$store.state;
-
-      let url = "http://localhost:8000/api/auth/login";
-      url += "?email=" + this.email;
-      url += "&password=" + this.password;
-      axios({
-        method: "post",
-        url: url,
-      })
-          .then((response) => {
-            console.log("in inlog, response", response);
-            localStorage.setItem('access_token', response.data.user.access_token)
-          })
-          .catch(function (error) {
-            console.log(error);
+    login() {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: this.email,
+          password: this.password,
+        })
+      };
+      fetch("http://127.0.0.1:8000/api/auth/login", requestOptions)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            if (data.user) {
+              localStorage.setItem("token", data.user.access_token);
+              this.$router.push(this.$route.query.redirect || '/');
+              this.$store.commit('login', {email : data.user.email, name : data.user.name});
+            }
           });
-      this.clearLogin();
-      this.dialogInlog = false;
-    },
-
-    clearLogin: function () {
-      this.password = "";
-      this.email = "";
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
 <style scoped>
