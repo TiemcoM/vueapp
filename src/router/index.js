c4d5c89f757d3a53013ad7ccaa3fc765907702c2
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-10-29 15:00:52 +0200

    Created users route

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import About from '../views/About.vue'
 import login from "@/components/login"
 import registreren from "@/components/registreren"
 import pizzas from "@/components/pizzas";
+import users from "@/components/users"
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,11 @@ const routes = [
     path: '/pizzas',
     name: 'pizzas',
     component: pizzas,
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: users,
   },
   {
     path: '/about',
