348158a4a0ededc5f77b38e18057ae589133d835
Tiemco <70142044+TiemcoM@users.noreply.github.com>
2021-10-29 15:03:07 +0200

    Created a list of registred users

---
+++ src/components/users.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <h1>Users list</h1>
+    <table>
+      <thead class="thead-dark">
+      <tr>
+        <th>ID</th>
+        <th>Naam</th>
+        <th>Email</th>
+        <th>Geregistreerd op</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+        <td>{{ user.created_at }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "users",
+
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted: function () {
+    let access_token = localStorage.getItem("token")
+    axios.get('http://127.0.0.1:8000/api/getallusers', {headers: {'Authorization': `Bearer ${access_token}`}})
+        .then(response => {
+          this.users = response.data
+          console.log(this.users)
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
